874a2f28c27cc028fe2ca4cb34037ce52a804ae8
RSLastra <raul.slastra@gmail.com>
2024-06-07 09:17:03 -0600

    feat: config appRouter

--- src/router/AppRouter.jsx
+++ src/router/AppRouter.jsx
@@ -1,7 +1,20 @@
-
+import { Routes, Route, Navigate } from "react-router-dom";
+import { LoginPage } from "../auth";
+import { CalendarPage } from "../calendar";
 
 export const AppRouter = () => {
+
+    const authStatus = 'not-authenticated';
+
     return (
-        <div>AppRouter</div>
+        <Routes>
+            //TODO
+            {
+                (authStatus === 'not-authenticated')
+                    ? < Route path='/auth/*' element={<LoginPage />} />
+                    : <Route path='/*' element={<CalendarPage />} />
+            }
+            <Route path='/*' element={<Navigate to='/auth/login' />} />
+        </Routes>
     )
 }
