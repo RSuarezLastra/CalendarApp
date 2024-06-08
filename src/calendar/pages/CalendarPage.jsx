1839d67a0d23786efa1c57ca190af1d5a218051b
RSLastra <raul.slastra@gmail.com>
2024-06-08 16:56:45 -0600

    feat: react-big-calendar config

--- src/calendar/pages/CalendarPage.jsx
+++ src/calendar/pages/CalendarPage.jsx
@@ -1,10 +1,56 @@
-import { Navbar } from "../"
+import { Calendar } from 'react-big-calendar';
+import 'react-big-calendar/lib/css/react-big-calendar.css'
+import { addHours } from 'date-fns'
+import { localizer, getMessagesEs } from '../../helpers';
+import { CalendarEvent, Navbar } from "../"
+
+
+const events = [{
+    title: 'Cumpleaños del jefe',
+    notes: 'Hay que comprar el pastel',
+    start: new Date(),
+    end: addHours(new Date(), 2),
+    bgColor: '#fafafa',
+    user: {
+        id: 1,
+        name: 'juan'
+    }
+
+}]
 
 export const CalendarPage = () => {
+
+    const evetStyleGetter = (event, start, end, isSelected) => {
+    
+        const style = {
+            backgroundColor: '#347CF7',
+            borderRadius: '0px',
+            opacity: 0.8,
+            color: 'white'
+        }
+
+        return {
+            style
+        }
+    }
+
     return (
         <>
             <Navbar />
-            <div>CalendarPage</div>
+
+            <Calendar
+                culture='es'
+                localizer={localizer}
+                events={events}
+                startAccessor="start"
+                endAccessor="end"
+                style={{ height: 'calc(100vh - 80px)' }}
+                messages={getMessagesEs()}
+                eventPropGetter={evetStyleGetter}
+                components={{
+                    event: CalendarEvent
+                }}
+            />
         </>
     )
 }
