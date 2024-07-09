291135e4f635d38c76117c2914c12e18dfda6566
RSLastra <raul.slastra@gmail.com>
2024-07-09 09:33:12 -0600

    feat(useAuthStore): startLogout

--- src/hooks/useAuthStore.js
+++ src/hooks/useAuthStore.js
@@ -63,6 +63,11 @@ export const useAuthStore = () => {
         }
     }
 
+    const startLogout = () => {
+        localStorage.clear();
+        dispatch(onLogout());
+    }
+
 
     return {
         status,
@@ -72,6 +77,7 @@ export const useAuthStore = () => {
         startLogin,
         startRegister,
         checkAuthToken,
+        startLogout,
     }
 
 }
