062169fb5b4adaaffabdb213ce2dfe468f5f2948
RSLastra <raul.slastra@gmail.com>
2024-07-16 09:20:29 -0600

    test(calendarApi): test axios client

--- tests/api/calendarApi.test.js
+++ tests/api/calendarApi.test.js
@@ -3,6 +3,20 @@ import { calendarApi } from "../../src/api/calendarApi";
 describe('Pruebas en calendarApi', () => {
 
     test('debe de tener la configuracion por defecto', () => {
-        console.log(calendarApi);
-    })
+
+        expect(calendarApi.defaults.baseURL).toBe(process.env.VITE_API_URL);
+    });
+
+    test('debe de tener le x-token en el header de las peticiones', async () => {
+
+        const token = 'FEF-223-ABC';
+        localStorage.setItem('token', token);
+
+        const res = await calendarApi.get('/auth')
+            .then(res => res)
+            .catch(res => res);
+
+        expect(res.config.headers['x-token']).toBe(token);
+
+    });
 });
