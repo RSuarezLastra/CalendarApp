83e2e4d2de0331b473da5f0d12fac9ea775d7283
RSLastra <raul.slastra@gmail.com>
2024-07-17 10:07:06 -0600

    test(authSlice): testing onLogin, onLogout and clearError

---
+++ tests/fixtures/testUser.js
@@ -0,0 +1,7 @@
+
+export const testUserCredentials = {
+    uid: "66928f2bd6816f050cc46047",
+    name: "TestUser",
+    email: "test@gmail.com",
+    password: "12345678"
+}
--- tests/store/auth/authSlice.test.js
+++ tests/store/auth/authSlice.test.js
@@ -1,5 +1,6 @@
-import { authSlice } from '../../../src/store';
-import { authStates, initialState } from '../../fixtures/authStates';
+import { authSlice, clearError, onLogin, onLogout } from '../../../src/store';
+import { authenticatedState, initialState, notAuthenticatedState } from '../../fixtures/authStates';
+import { testUserCredentials } from '../../fixtures/testUser';
 
 describe('Pruebas en authSlice', () => {
 
@@ -9,9 +10,35 @@ describe('Pruebas en authSlice', () => {
 
     });
 
-    test('debe de cambiar el isDateModalOpen correctamente ', () => {
+    test('debe de realizar login', () => {
 
+        const state = authSlice.reducer(initialState, onLogin(testUserCredentials));
+        
+        expect(state).toEqual({
+            status: 'authenticated',
+            user: testUserCredentials,
+            errorMessage: undefined
+        })
 
+    });
+    test('debe de realizar logout', () => {
+        const errorMessage = 'Credenciales no validas'
+        const state = authSlice.reducer(authenticatedState, onLogout(errorMessage));
 
+        expect(state).toEqual({
+            status: 'not-authenticated',
+            user: {},
+            errorMessage,
+        });
+
+    });
+
+    test('debe de limpiar el mensaje de error', ()=> {
+
+        const errorMessage = 'Credenciales no validas'
+        const state = authSlice.reducer(authenticatedState, onLogout(errorMessage));
+        const newState = authSlice.reducer(state, clearError());
+
+        expect(newState.errorMessage).toBe(undefined);
     });
 });
