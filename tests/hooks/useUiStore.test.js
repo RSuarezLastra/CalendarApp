35ecd61d739085fb82f521f3260de973c603904c
RSLastra <raul.slastra@gmail.com>
2024-07-22 10:01:21 -0600

    test(useUiStore): testing openDateModal function

--- tests/hooks/useUiStore.test.js
+++ tests/hooks/useUiStore.test.js
@@ -1,6 +1,6 @@
 import { Provider } from "react-redux";
 import { configureStore } from "@reduxjs/toolkit";
-import { renderHook } from "@testing-library/react";
+import { renderHook, act } from "@testing-library/react";
 import { useUiStore } from "../../src/hooks";
 import { uiSlice } from "../../src/store";
 
@@ -20,16 +20,34 @@ describe('pruebas en el useUiStore', () => {
 
     test('debe de regresar los valores por defecto', () => {
 
-        const mockStore = getMockStore({isDateModalOpen: false})
+        const mockStore = getMockStore({ isDateModalOpen: false })
 
         const { result } = renderHook(() => useUiStore(), {
             wrapper: ({ children }) => <Provider store={mockStore}>{children}</Provider>
         });
-        
+
         expect(result.current).toEqual({
             isDateModalOpen: false,
             openDateModal: expect.any(Function),
             closeDateModal: expect.any(Function)
         });
-    }); 
+    });
+
+    test('openDateModal debe de colocar true en el isDateModalOpen', () => {
+
+        const mockStore = getMockStore({ isDateModalOpen: false })
+
+        const { result } = renderHook(() => useUiStore(), {
+            wrapper: ({ children }) => <Provider store={mockStore}>{children}</Provider>
+        });
+
+        const { openDateModal } = result.current;
+
+        act(() => {
+            openDateModal();
+        });
+
+        expect(result.current.isDateModalOpen).toBeTruthy();
+
+    });
 });
