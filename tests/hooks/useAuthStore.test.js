3a76868c91863e92868718dab1fb3520dc86926a
RSLastra <raul.slastra@gmail.com>
2024-07-24 09:21:04 -0600

    test(useAuthStore): testign starlogin with incorrect credentials

--- tests/hooks/useAuthStore.test.js
+++ tests/hooks/useAuthStore.test.js
@@ -1,6 +1,6 @@
 import { Provider } from "react-redux";
 import { configureStore } from "@reduxjs/toolkit";
-import { act, renderHook } from "@testing-library/react";
+import { act, renderHook, waitFor } from "@testing-library/react";
 import { authSlice } from "../../src/store";
 import { initialState, notAuthenticatedState } from "../fixtures/authStates";
 import { useAuthStore } from "../../src/hooks";
@@ -53,7 +53,7 @@ describe('Pruebas en el useAuthStore', () => {
 
         const { errorMessage, user, status } = result.current;
 
-        expect({errorMessage, user, status}).toEqual({
+        expect({ errorMessage, user, status }).toEqual({
             status: 'authenticated',
             user: { uid: '66928f2bd6816f050cc46047', name: 'TestUser' },
             errorMessage: undefined,
@@ -63,4 +63,35 @@ describe('Pruebas en el useAuthStore', () => {
         expect(localStorage.getItem('token-init-date')).toEqual(expect.any(String));
 
     });
+
+    test('startLogin debe fallar', async () => {
+        localStorage.clear();
+
+        const mockStore = getMockStore({ ...notAuthenticatedState });
+        const { result } = renderHook(() => useAuthStore(), {
+            wrapper: ({ children }) => <Provider store={mockStore}> {children} </Provider>
+        })
+
+        await act(async () => {
+            await result.current.startLogin({ email: 'reer@fef.com', password: '12345678' })
+        });
+
+        const { status, user, errorMessage } = result.current;
+
+        expect({ status, user, errorMessage }).toEqual({
+            status: 'not-authenticated',
+            user: {},
+            errorMessage: 'Credenciales incorrectas',
+        })
+
+        await waitFor(
+            () => expect(result.current.errorMessage).toBe(undefined)
+        );
+
+        expect(localStorage.getItem('token')).toBeNull();
+        expect(localStorage.getItem('token-init-date')).toBeNull();
+
+    });
+
+
 });
